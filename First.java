0f63042de0616a2414018bf37b5e23b9f3c7d6ff
ssonawane511 <ssonawane511@gmail.com>
2021-01-07 22:25:35 +0530

    added the first println program

---
+++ First.java
@@ -0,0 +1,21 @@
+public class First {
+    public static void main(String[] args) {
+        String name = "sagar sonawan"; 
+        int myNum ; 
+        myNum = 245 ;
+        float myFloat = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true; 
+        String bhadwa= "abhisheck bhoyar";
+
+        //for better understanding use pascal scale
+
+
+        System.out.println(name);
+        System.out.println(myNum);
+        System.out.println(myFloat);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        // helllo muther fucker
+    }
+}
